1e8b81dac30ec3e5d314bf117eb24fb6ea098e80
deekev <deekev@gmail.com>
2021-10-26 15:43:26 +0200

    week03day02

---
+++ week-03-lab-tasks/src/main/java/day02/Mathematics.java
@@ -0,0 +1,17 @@
+package day02;
+
+public class Mathematics {
+
+    public boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        } else {
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
